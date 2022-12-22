88473282195b5655d6dbe08c96d0e1313026c6ba
Meshcheryakova <lmavi.kisune@gmail.com>
2022-12-22 18:22:29 +0400

    Initial commit

---
+++ src/main/java/Product/Serv/ProductServlet.java
@@ -0,0 +1,10 @@
+package Product.Serv;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+@WebServlet("/start")
+public class ProductServlet extends HttpServlet {
+
+}
