9afb00f4d0d905052658fa47d6f931ea585cf384
Meshcheryakova <lmavi.kisune@gmail.com>
2022-12-26 21:05:08 +0400

    Product initial №3

---
+++ Spring_web/src/main/java/Controller/Product_Repository.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import com.example.Spring_web.Product;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Product_Repository {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init(){
+        products = new ArrayList<>(Arrays.asList());
+        products.add(new Product(1,"Сахар",90.04));
+        products.add(new Product(2,"Картофель",28.55));
+        products.add(new Product(3,"Гречневая мука",67.78));
+        products.add(new Product(4,"Рис длиннозерновой",119.40));
+        products.add(new Product(5,"Капуста",18.25));
+    }
+
+    public Product findById(int id){
+        return products.stream().filter(p -> p.getId()==id).findFirst().orElseThrow(()->new RuntimeException("product not found"));
+    }
+
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(products);
+    }
+
+    public void addProduct(Product product) {
+    }
+}
---
+++ Spring_web/src/main/java/com/example/Spring_web/Product.java
@@ -0,0 +1,31 @@
+package com.example.Spring_web;
+public class Product {
+    private int id;
+    private String title;
+    private double cost;
+
+    public Product(int id, String title, double cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", product name='" + title + ", cost=" + cost;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getCost() {
+        return  cost;
+    }
+}
+
+
